124a34dd7b03fc7461792e80acf9b6993cac04ff
arieldiazsaman <arieldiazsamaniego@gmail.com>
2024-02-22 01:22:14 -0300

    WebsiteAScraper methods implementation

--- src/WebsiteAScraper.js
+++ src/WebsiteAScraper.js
@@ -4,6 +4,32 @@ class WebsiteAScraper extends WebsiteCrawler {
     constructor() {
         super();
     }
+
+    async getCurrentPageTitle() {
+        return this.page.title();
+    }
+
+    async getProductNames() {
+        return await this.page.evaluate(() => {
+            return Array.from(
+                document.querySelectorAll('.product-item-info')
+            ).map(
+                (product) => product.querySelector('.product.name.product-item-name').innerText
+            )
+        })
+    }
+
+    async getProductPrices() {
+        const productNames = await this.getProductNames();
+        return await this.page.evaluate((productNames) => {
+            return Array.from(
+                document.querySelectorAll('.product-item-info')
+            ).map((product, index) => ({
+                product: productNames[index],
+                price: product.querySelector('.price').innerText,
+            }))
+        }, productNames)
+    }
 }
 
 module.exports = WebsiteAScraper;
---
+++ src/app.js
@@ -0,0 +1,13 @@
+const WebsiteAScraper = require('./WebsiteAScraper');
+
+(async () => {
+    const nisseiPage = new WebsiteAScraper();
+    await nisseiPage.openBrowser();
+    await nisseiPage.goToPage('https://nissei.com/py/bebidas/alimentos/chocolates/');
+    const pageTitle = await nisseiPage.getCurrentPageTitle();
+    const productNames = await nisseiPage.getProductNames();
+    const productNamesAndPrices = await nisseiPage.getProductPrices();
+    console.log(`Title of the current page: ${ pageTitle }`);
+    console.log(`List of product names on the page: ${ JSON.stringify(productNames) }`);
+    console.log(`Price of each product: ${ JSON.stringify(productNamesAndPrices) }`);
+})();
